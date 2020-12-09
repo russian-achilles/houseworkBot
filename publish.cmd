0e64a282f4c63975448a1c80f64112e646cf2646
russian-achilles <o232079t@mail.kyutech.jp>
2020-12-09 13:11:11 +0900

    initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=.\houseworkbot-Web-Deploy.pubxml -p:Password=biifa9MKepYg7JsEJ94Z8T2nPnY1xamhkLtPeY88GdF1F9JqbBiGHoiZ1Cbg
+
